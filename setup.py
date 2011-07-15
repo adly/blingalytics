ec0b69298e3fa58de2a06fa69371d2d019308515
Jeff Schenck <jmschenck@gmail.com>
2011-07-14 17:13:15 -0700

    Updates setup.py options

--- setup.py
+++ setup.py
@@ -1,18 +1,31 @@
 from distutils.core import setup
 
 
+readme = open('README.rst').read()
+
 setup(
     name='Blingalytics',
     version='0.1dev',
     author='Jeff Schenck, Adly Inc.',
-    author_email='jeff@adly.com',
-    url='http://blingalytics.com/',
+    author_email='jmschenck@gmail.com',
+    url='http://github.com/adly/blingalytics',
     description='Blingalytics is a tool for building reports from your data.',
+    long_description=readme,
     license='',
     packages=[
         'blingalytics',
         'blingalytics.caches',
         'blingalytics.sources',
         'blingalytics.utils',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Programming Language :: Python',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Office/Business',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Scientific/Engineering',
     ],
 )
