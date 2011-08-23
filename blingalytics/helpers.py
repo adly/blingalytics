56c4848e5b67dd5e33e320b5474596affd994906
Jeff Schenck <jmschenck@gmail.com>
2011-08-23 16:35:28 -0700

    Makes frontend better styled and more robust

--- blingalytics/helpers.py
+++ blingalytics/helpers.py
@@ -1,19 +1,51 @@
 import json
 
+from blingalytics import get_report_by_code_name
+from blingalytics.caches import local_cache
 
-def process_report_response(report, params):
-    """
-    Pass in the report instance and the request parameters. This will return
-    the appropriate JSON to return for the AJAX response.
-    """
-    # If request is for headers, return them
-    if params.get('metadata'):
+
+DEFAULT_CACHE = local_cache.LocalCache()
+
+def report_response(params, runner=None, cache=DEFAULT_CACHE):
+    # Find and instantitate the report class
+    params = dict((k, v) for k, v in params.items())
+    report_code_name = params.pop('report', None)
+    if not report_code_name:
+        return json.dumps({'errors': ['Report code name not specified.']})
+    report_cls = get_report_by_code_name(report_code_name)
+    if not report_cls:
+        return json.dumps({'errors': ['Specified report not found.']})
+    report = report_cls(cache)
+
+    # Return immediately for metadata request
+    if params.pop('metadata', False):
         return json.dumps({
+            'errors': [],
+            'widgets': report.render_widgets(),
             'header': report.report_header(),
             'default_sort': report.default_sort,
         })
 
-    # Otherwise, request is for rows
+    # Process user inputs
+    errors = report.clean_user_inputs(**params)
+    if errors:
+        return json.dumps({
+            'errors': [str(error) for error in errors],
+        })
+
+    # Run the report, either synchronously or not, if needed
+    if not report.is_report_finished():
+        if runner:
+            if not report.is_report_started():
+                runner(report_code_name, params)
+            return json.dumps({
+                'errors': [],
+                'poll': True,
+            })
+        else:
+            report.run_report()
+
+    # Return report data
     offset = int(params.get('iDisplayStart'))
     limit = int(params.get('iDisplayLength'))
     sort_col = params.get('iSortCol_0')
@@ -25,8 +57,11 @@ def process_report_response(report, params):
     sort = (sort_col, sort_dir)
     echo = int(params.get('sEcho'))
     return json.dumps({
+        'errors': [],
+        'poll': False,
         'iTotalRecords': report.report_row_count(),
         'iTotalDisplayRecords': report.report_row_count(),
         'sEcho': str(echo),
         'aaData': report.report_rows(sort=sort, limit=limit, offset=offset),
+        'footer': report.report_footer(),
     })
--- blingalytics/statics/js/jquery.blingalytics.js
+++ blingalytics/statics/js/jquery.blingalytics.js
@@ -9,13 +9,24 @@
 
     return this.each(function() {
       var container = $(this);
+      container.empty().addClass('bl_container');
+      var errors_ul = $('<ul class="errors"></ul>');
+      errors_ul.prependTo(container);
       var params = {metadata: '1', report: settings.reportCodeName};
       $.getJSON(settings.url, params, function(metadata) {
+        if (metadata.errors.length) {
+          errors_ul.empty();
+          for (var i in metadata.errors) {
+            errors_ul.append('<li>' + metadata.errors[i] + '</li>');
+          }
+          return;
+        }
         var headers = metadata.header;
         var defaultSort = metadata.default_sort;
         var columns = [];
         var classes = [];
         var thead = '';
+        var tfoot = '';
         for (var i in headers) {
           columns.push({
             'bSearchable': false,
@@ -29,18 +40,32 @@
           colClasses = headers[i].hidden ? colClasses + ' bl_hidden' : colClasses;
           classes.push(colClasses);
           thead += '<th class="' + colClasses + '">' + headers[i].label + '</th>';
+          tfoot += '<th class="' + colClasses + '"></th>';
           if (headers[i].key == defaultSort[0]) {
             defaultSort = ([parseInt(i), defaultSort[1]]);
           }
         }
 
+        // Construct the widgets
+        var widgets_container = $('<ul class="widgets"></ul>');
+        container.append(widgets_container);
+        for (var i in metadata.widgets) {
+          widgets_container.append('<li>' + metadata.widgets[i] + '</li>');
+        }
+        $('<button>Run Report</button>').appendTo(widgets_container).click(function() {
+          datatable.fnSort([defaultSort]);
+        });
+
         // Construct the skeleton of the table
         var table = $(
-          '<table class="bl_table display"><thead>' +
-          '<tr>' + thead + '</tr>' +
-          '</thead><tbody></tbody></table>'
+          '<table class="bl_table display">' +
+          '<thead><tr>' + thead + '</tr></thead>' +
+          '<tfoot><tr>' + tfoot + '</tr></tfoot>' +
+          '<tbody></tbody>' +
+          '</table>'
         );
-        container.empty().append(table);
+        container.append(table);
+
         // Init the datatable widget
         var datatable = table.dataTable({
           aoColumnDefs: columns,
@@ -53,7 +78,7 @@
           bServerSide: true,
           sAjaxSource: settings.url + '?report=' + settings.reportCodeName,
           aaSorting: [defaultSort],
-          iDeferLoading: 100,
+          iDeferLoading: 0,
           iDisplayLength: 25,
           fnRowCallback: function(nRow, aData, iDisplayIndex) {
             var children = nRow.children;
@@ -61,9 +86,38 @@
               children[i].className += classes[i];
             }
             return nRow;
+          },
+          fnServerData: function(sSource, aoData, fnCallback) {
+            var url = sSource + '&' + widgets_container.find('input, select, textarea').serialize();
+            $.getJSON(url, aoData, function(data) {
+              if (data.errors.length) {
+                errors_ul.empty();
+                for (var i in data.errors) {
+                  errors_ul.append('<li>' + data.errors[i] + '</li>');
+                }
+                fnCallback({
+                  'iTotalRecords': 0,
+                  'iTotalDisplayRecords': 0,
+                  'aaData': [],
+                });
+              } else {
+                var footer_tds = table.find('tfoot th');
+                for (var i in data.footer) {
+                  footer_tds.eq(i).html(data.footer[i]);
+                }
+                fnCallback(data);
+              }
+            });
+          },
+          fnHeaderCallback: function(nHead, aasData, iStart, iEnd, aiDisplay) {
+            // Fix missing header sorting classes
+            var head = $(nHead);
+            console.log(head.find('th'));
+            head.find('th').removeClass('sorting');
+            console.log(head.find('th:has(.ui-icon-triangle-1-n, .ui-icon-triangle-1-s)'));
+            head.find('th:has(.ui-icon-triangle-1-n, .ui-icon-triangle-1-s)').addClass('sorting');
           }
         });
-        datatable.fnSort([defaultSort]);
       });
     });
   };
