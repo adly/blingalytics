a039c9952f1d30a61b6ccf50ab598e70d0b2c405
Jeff Schenck <jmschenck@gmail.com>
2011-08-25 18:00:06 -0700

    Rev version number

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ open-source project. Blingalytics is released under the `MIT License`_.
 
 setup(
     name='Blingalytics',
-    version='0.1.1dev',
+    version='0.1.2dev',
     author='Jeff Schenck, Adly Inc.',
     author_email='jmschenck@gmail.com',
     url='http://github.com/adly/blingalytics',
